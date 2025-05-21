a3c17fd22b8dcf43e30ae1a9e17b3668044c6ea8
goong <cookie_31@naver.com>
2025-05-22 01:01:06 +0900

    first commit

---
+++ src/main/java/com/example/zeoro_base/ZeoroBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeoro_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeoroBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeoroBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zeoro_base/ZeoroBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeoro_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeoroBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
